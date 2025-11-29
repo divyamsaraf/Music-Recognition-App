c8fdac27e0e2ffbee196ad3e5ebc6a30a84cae7d
divyamsaraf <divyam.saraf@gmail.com>
2025-11-29 02:14:33 -0800

    Add rewrite for Python API to ensure routing

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/recognize_py',
+                destination: '/api/recognize_py',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
